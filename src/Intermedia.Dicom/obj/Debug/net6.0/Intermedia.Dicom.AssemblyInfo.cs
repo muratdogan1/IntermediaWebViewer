25b2081a7fb1632b51cefc299a9b0c81cf513f8a
murat dogan <muratdogan@hotmail.com>
2026-02-27 09:38:28 +0300

    genel proje iyileştirmeleri

--- src/Intermedia.Dicom/obj/Debug/net6.0/Intermedia.Dicom.AssemblyInfo.cs
+++ src/Intermedia.Dicom/obj/Debug/net6.0/Intermedia.Dicom.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Intermedia.Dicom")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acd06def0db869743b4e5c98e92800035dbb0bdc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1865197a9a452c5aadfa9fec000114395e48b6af")]
 [assembly: System.Reflection.AssemblyProductAttribute("Intermedia.Dicom")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Intermedia.Dicom")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
